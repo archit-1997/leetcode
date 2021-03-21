98657dfa9d94bea59d3e022be72f1b16b4ed8a4f
Archit Singh <architsingh456@gmail.com>
2021-03-22 01:14:30 +0530

    UPD: added solution to some daily problems and a general problem

---
+++ January_LeetCoding_Challenge_2021/boatsToSavePeople.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n=people.size();
+        sort(people.begin(),people.end());
+        int l=0,r=n-1;
+        int ans=0;
+        while(r>=0 && people[r]==limit){
+            ans++;
+            r--;
+        }
+        if(r<0)
+            return ans;
+        if(r==0)
+            return ans+1;
+        while(l<=r){
+            if(l==r)
+                return ans+1;
+            if(people[l]+people[r]<=limit){
+                ans++;
+                l++;
+                r--;
+            }
+            else{
+                r--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
