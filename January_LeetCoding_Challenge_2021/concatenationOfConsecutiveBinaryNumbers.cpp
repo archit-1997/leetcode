1279f5e9ef00d8d07a3202c0196d68b6831e86ec
Archit Singh <architsingh456@gmail.com>
2021-05-16 19:57:12 +0530

    UPD : formatted

--- January_LeetCoding_Challenge_2021/concatenationOfConsecutiveBinaryNumbers.cpp
+++ January_LeetCoding_Challenge_2021/concatenationOfConsecutiveBinaryNumbers.cpp
@@ -3,25 +3,24 @@ using namespace std;
 
 class Solution {
 public:
-
-    long long int countBits(int n){
-        long long int count=0;
-        while(n!=0){
-            n/=2;
-            count++;
-        }
-        return count;
+  long long int countBits(int n) {
+    long long int count = 0;
+    while (n != 0) {
+      n /= 2;
+      count++;
     }
+    return count;
+  }
 
-    int concatenatedBinary(int n) {
-        long long int ans=1,mod=1e9+7;
-        for(int i=2;i<=n;i++){
-            long long int shift= countBits(i);
-            ans=ans<<shift;
-            ans=ans%mod;
-            ans+=i;
-            ans=ans%mod;
-        }
-        return ans;
+  int concatenatedBinary(int n) {
+    long long int ans = 1, mod = 1e9 + 7;
+    for (int i = 2; i <= n; i++) {
+      long long int shift = countBits(i);
+      ans = ans << shift;
+      ans = ans % mod;
+      ans += i;
+      ans = ans % mod;
     }
+    return ans;
+  }
 };
