35c3b0439efbe5755be4d6d585f21541277ba8f0
Archit Singh <architsingh456@gmail.com>
2021-05-17 09:21:03 +0530

    UPD : added solution for minimize deviation in array

--- January_LeetCoding_Challenge_2021/minimizeDeviationInArray.cpp
+++ January_LeetCoding_Challenge_2021/minimizeDeviationInArray.cpp
@@ -1,4 +1,33 @@
-//
-// Created by Archit Singh on 16/05/21.
-//
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution {
+public:
+  int minimumDeviation(vector<int> &nums) {
+    int curMin = INT_MAX;
+    priority_queue<int> q;
+    for (int n : nums) {
+      if (n % 2 == 1)
+        n *= 2;
+      curMin = min(curMin, n);
+      q.push(n);
+    }
+
+    int ans = INT_MAX;
+
+    while (1) {
+      int num = q.top();
+      q.pop();
+
+      ans = min(ans, num - curMin);
+
+      if (num % 2 == 1)
+        break;
+
+      curMin = min(curMin, num / 2);
+      q.push(num / 2);
+    }
+
+    return ans;
+  }
+};
