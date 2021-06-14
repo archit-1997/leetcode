13216bd3078b1d063226615ddb1f7481f5522d21
Archit Singh <architsingh456@gmail.com>
2021-06-14 11:43:03 +0530

    UPD : added solutions to problems

--- May_LeetCoding_Challenge_2021/jumpGameII.cpp
+++ May_LeetCoding_Challenge_2021/jumpGameII.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-  int jump(vector<int> &nums) {
-    // using dp to solve this problem
-    int n = nums.size();
-    // vector<int> dp(n,INT_MAX)
-    // this will given runtime error as overflow : INT_MAX+1
-    vector<int> dp(n, 1001);
-    // last argument is the starting index
-    // this will min number of jumps required to reach the
-    // last index from 0 index
-    return solve(nums, dp, 0);
-  }
 
-  int solve(vector<int> &nums, vector<int> &dp, int pos) {
-    // reached the final pos
-    if (pos >= nums.size() - 1)
-      return 0;
-    // already visited
-    if (dp[pos] != 1001)
-      return dp[pos];
-    // reach all posssible places from this pos
-    // recursively calculate for this position
-    for (int i = 1; i <= nums[pos]; i++) {
-      dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + i));
+    int countSteps(vector<int> &nums,vector<int> &dp, int index){
+        int n=nums.size();
+        if(index>=n-1)
+            return 0;
+
+        if(dp[index]!=10001)
+            return dp[index];
+
+        for(int i=index+1;i<=nums[index]+index;i++)
+            dp[index]=min(dp[index],1+countSteps(nums,dp,i));
+
+        return dp[index];
     }
-    return dp[pos];
-  }
+
+
+
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        //dp[i] indicates min num of steps required to reach n-1 from here
+
+        vector<int> dp(n,10001);
+
+        return countSteps(nums,dp,0);
+
+
+    }
 };
