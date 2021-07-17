9ca5e71e470fd65c684b60e7e04f091f09b360e3
archit-1997 <architsingh456@gmail.com>
2021-07-17 07:25:53 +0530

    UPD : added solution to July Leetcode Challenge

---
+++ July_LeetCoding_Challenge_2021/findPeakElement.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(nums[m]<nums[m+1])
+                l=m+1;
+            else
+                r=m;
+        }
+        return l;
+    }
+};
