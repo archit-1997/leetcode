d86f5d658c59656fe16f225590e9aa349c95bd35
archit-1997 <architsingh456@gmail.com>
2021-07-19 09:29:04 +0530

    UPD : segregated problems based on their category

---
+++ July_LeetCoding_Challenge_2021/findMedianFromDataStream.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef tree<int,null_type,less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> new_data_set;
+class MedianFinder {
+public:
+    ordered_set q;
+    int size=0;
+    /** initialize your data structure here. */
+    MedianFinder() {
+       if(size()%2==1){
+            
+       } 
+    }
+    
+    void addNum(int num) {
+        
+    }
+    
+    double findMedian() {
+        
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
+
