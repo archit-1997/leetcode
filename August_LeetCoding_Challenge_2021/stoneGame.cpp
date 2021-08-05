f17f2202629cdbb6e7de98e4d9639a8ee7ce3e6d
archit-1997 <architsingh456@gmail.com>
2021-08-05 22:34:05 +0530

    UPD : added solution for Stone Game

---
+++ August_LeetCoding_Challenge_2021/stoneGame.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : stoneGame.cpp
+ * @created     : Thursday Aug 05, 2021 22:13:44 IST
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
