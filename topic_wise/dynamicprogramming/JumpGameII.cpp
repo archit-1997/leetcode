51fbd90db5c844fa19b52708385b9c35407d7454
archit-1997 <architsingh456@gmail.com>
2021-08-05 15:09:43 +0530

    UPD : added solution for Jump Game and Jump Game II

--- topic_wise/dynamicprogramming/JumpGameII.cpp
+++ topic_wise/dynamicprogramming/JumpGameII.cpp
@@ -17,7 +17,6 @@ public:
         if(index>=n-1)
             return 0;
 
-        //if we know the min of this index, then simply return this value
         if(dp[index]!=10001)
             return dp[index];
 
@@ -27,7 +26,6 @@ public:
             dp[index]=min(dp[index],1+findMinSteps(nums,dp,i));
         }
 
-        //return the final answer
         return dp[index];
 
 
@@ -37,10 +35,9 @@ public:
         int n=nums.size();
 
         //dp[i]-> min number of steps required to reach n-1 from index i
-        //initialised as 10001 as the max number of steps that will be required is 10001 is every step is 1
         vector<int> dp(n,10001);
-        //NOTE : initialising to INT_MAX will give integer overflow
 
         return findMinSteps(nums,dp,0);
     }
 };
+
--- topic_wise/dynamicprogramming/jumpGame.cpp
+++ topic_wise/dynamicprogramming/jumpGame.cpp
@@ -1,21 +1,37 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : jumpGame.cpp
+ * @created     : Thursday Aug 05, 2021 09:31:09 IST
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-  bool canJump(vector<int> &nums) {
-    int n = nums.size();
-    int mr = 0;
-    if (n == 1)
-      return true;
-    for (int i = 0; i < n; i++) {
-      // if we have gone past this element or we are at this element
-      if (i > mr)
-        return false;
-      if (nums[i] == 0)
-        continue;
 
-      mr = max(mr, i + nums[i]);
-      if (mr >= n - 1)
-        return true;
+    int isReachable(vector<int> &nums,vector<int> &dp,int index){
+        int n=nums.size();
+        if(index==n-1)
+            return 0;
+
+        if(dp[index]!=10001)
+            return dp[index];
+
+        for(int i=index+1;i<nums[index]+index;i++){
+            dp[index]=min(dp[index],1+isReachable(nums,dp,i));
+        }
+
+        return dp[index];
     }
-    return false;
-  }
+
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,10001);
+        //min if we can reach the last index from 0th index
+        isReachable(nums,dp,0);
+        return dp[n-1]!=10001;
+    }
 };
