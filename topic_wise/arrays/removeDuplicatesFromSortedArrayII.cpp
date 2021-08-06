97f08763088d548af0d7e7e6ffde7d490a178514
archit-1997 <architsingh456@gmail.com>
2021-08-06 08:12:39 +0530

    UPD : added solution for Remove Duplicates From Sorted Array II

---
+++ topic_wise/arrays/removeDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,43 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : removeDuplicatesFromSortedArrayII.cpp
+ * @created     : Friday Aug 06, 2021 07:19:19 IST
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+            return 1;
+        int index=1,val=nums[0],count=1,i=1;
+
+        while(i<n){
+            if(nums[i]==val){
+                count++;
+                if(count>2){
+                    while(i<n && nums[i]==val)
+                        i++;
+                    //if we have gone out of range, then break
+                    if(i==n)
+                        return index;
+                    count=1;
+                    val=nums[i];
+                }
+            }
+            else{
+                //we have found a new number
+                val=nums[i];
+                count=1;
+            }
+            swap(nums[index++],nums[i++]);
+        }
+        
+        return index;
+    }
+};
