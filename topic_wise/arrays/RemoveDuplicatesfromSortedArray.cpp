65050b7cde605940130dd26efa10376d2ab46037
archit-1997 <architsingh456@gmail.com>
2021-08-06 08:15:04 +0530

    UPD : added solution for Remove Duplicates From Sorted Array

--- topic_wise/arrays/RemoveDuplicatesfromSortedArray.cpp
+++ topic_wise/arrays/RemoveDuplicatesfromSortedArray.cpp
@@ -1,51 +1,46 @@
-/*Given a sorted array nums, remove the duplicates in-place such that each
-element appear only once and return the new length.
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : removeDuplicatesFromSortedArray.cpp
+ * @created     : Friday Aug 06, 2021 07:19:19 IST
+ */
 
-Do not allocate extra space for another array, you must do this by modifying the
-input array in-place with O(1) extra memory.
-
-Example 1:
-
-Given nums = [1,1,2],
-
-Your function should return length = 2, with the first two elements of nums
-being 1 and 2 respectively.
-
-It doesn't matter what you leave beyond the returned length.
-Example 2:
-
-Given nums = [0,0,1,1,1,2,2,3,3,4],
-
-Your function should return length = 5, with the first five elements of nums
-being modified to 0, 1, 2, 3, and 4 respectively.
-
-It doesn't matter what values are set beyond the returned length.
-Clarification:
-
-Confused why the returned value is an integer but your answer is an array?
-
-Note that the input array is passed in by reference, which means modification to
-the input array will be known to the caller as well.
-
-Internally you can think of this:
-
-// nums is passed in by reference. (i.e., without making a copy)
-int len = removeDuplicates(nums);
-
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}*/
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
-  int removeDuplicates(vector<int> &nums) {
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int index=1,val=nums[0],count=1,i=1;
 
-    auto it = unique(nums.begin(), nums.end());
-
-    nums.erase(it, nums.end());
-
-    return nums.size();
-  }
+        while(i<n){
+            if(nums[i]==val){
+                count++;
+                if(count>1){
+                    while(i<n && nums[i]==val)
+                        i++;
+                    //if we have gone out of range, then break
+                    if(i==n)
+                        return index;
+                    count=1;
+                    val=nums[i];
+                }
+            }
+            else{
+                //we have found a new number
+                val=nums[i];
+                count=1;
+            }
+            swap(nums[index++],nums[i++]);
+        }
+        
+        return index;
+    }
 };
+
