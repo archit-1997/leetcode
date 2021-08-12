632d2f9c525cba4492b2035fb303dcc4cb806520
archit-1997 <architsingh456@gmail.com>
2021-08-12 22:29:15 +0530

    UPD :updated solution for Sum Of Left Leaves

--- topic_wise/trees/sumOfLeftLeaves.cpp
+++ topic_wise/trees/sumOfLeftLeaves.cpp
@@ -1,38 +1,44 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : sumOfLeftLeaves.cpp
+ * @created     : Thursday Aug 12, 2021 22:26:54 IST
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
 
-    bool isLeaf(TreeNode *root) {
-        return (!root->left && !root->right);
+    bool isLeaf(TreeNode* root){
+        return (root->left==NULL && root->right==NULL);
     }
 
-    void findSum(TreeNode *root, int &ans) {
-        if (root == NULL)
-            return;
-        if (root->left && isLeaf(root->left))
-            ans += root->left->val;
-        else
-            findSum(root->left, ans);
 
-        findSum(root->right, ans);
-    }
-
-    int sumOfLeftLeaves(TreeNode *root) {
-        if (isLeaf(root))
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root==NULL)
             return 0;
 
-        int ans = 0;
-        findSum(root, ans);
+        int ans=0;
+        if(root->left){
+            if(isLeaf(root->left))
+                ans+=root->left->val;
+            else
+                ans+=sumOfLeftLeaves(root->left);
+        }
+        ans+=sumOfLeftLeaves(root->right);
+
         return ans;
     }
 };
