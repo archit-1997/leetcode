5e5e206a2662bce8361307f8f50809c2fd9c9d63
archit-1997 <architsingh456@gmail.com>
2021-08-14 19:56:01 +0530

    UPD : updated solution for Lowest Common Ancestor of a Binary Search Tree

--- topic_wise/trees/lowestCommonAncestorOfABinarySearchTree.cpp
+++ topic_wise/trees/lowestCommonAncestorOfABinarySearchTree.cpp
@@ -1,23 +1,29 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : lowestCommonAncestorOfABinarySearchTree.cpp
+ * @created     : Saturday Aug 14, 2021 19:52:44 IST
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
 
 class Solution {
 public:
-    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-        if (p->val > root->val && q->val > root->val)
-            return lowestCommonAncestor(root->right, p, q);
-        else if (p->val < root->val && q->val < root->val)
-            return lowestCommonAncestor(root->left, p, q);
-        return root;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val > root->val && q->val > root->val)
+            return  lowestCommonAncestor(root->right,p,q);
+        else if(p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left,p,q);
+        else
+            return root;
     }
-};//
-// Created by Archit Singh on 20/06/21.
-//
-
+};
