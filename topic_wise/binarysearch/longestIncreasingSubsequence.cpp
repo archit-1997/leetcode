9b146f87f3e4d311702023b0e150d3df999f894b
archit-1997 <architsingh456@gmail.com>
2021-08-19 13:58:52 +0530

    UPD : updated solution for Longest Increasing Subsequence

--- topic_wise/binarysearch/longestIncreasingSubsequence.cpp
+++ topic_wise/binarysearch/longestIncreasingSubsequence.cpp
@@ -3,7 +3,7 @@
  * @GitHub      : archit-1997
  * @Email       : architsingh456@gmail.com
  * @file        : longestIncreasingSubsequence.cpp
- * @created     : Sunday Aug 08, 2021 21:53:16 IST
+ * @created     : Thursday Aug 19, 2021 13:53:35 IST
  */
 
 #include <bits/stdc++.h>
@@ -13,7 +13,10 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            //we find the lower bound of this element
             int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
             if(index==ans.size())
                 ans.push_back(nums[i]);
