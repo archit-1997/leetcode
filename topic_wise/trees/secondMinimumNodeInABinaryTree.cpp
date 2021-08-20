fab4e49c05ba62c6dec9b21b578102b94059ae8f
archit-1997 <architsingh456@gmail.com>
2021-08-20 21:33:24 +0530

    UPD : updated solution for Second Minimum Node in a Binary Tree

--- topic_wise/trees/secondMinimumNodeInABinaryTree.cpp
+++ topic_wise/trees/secondMinimumNodeInABinaryTree.cpp
@@ -1,34 +1,50 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : secondMinimumNodeInABinaryTree.cpp
+ * @created     : Friday Aug 20, 2021 21:18:33 IST
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
 class Solution {
 public:
-    void inorder(TreeNode *root, set<int> &res) {
-        if (root == NULL)
+
+    void inorder(TreeNode* root,long long int &first,long long int &second){
+        if(root==NULL)
             return;
+         inorder(root->left,first,second);
+         if(root->val<first){
+            second=first;
+            first=root->val;
+         }
+         else if(root->val>first){
+            if(root->val<second)
+                second=root->val;
+         }
 
-        inorder(root->left, res);
-        res.insert(root->val);
-        inorder(root->right, res);
-
+         inorder(root->right,first,second);
     }
 
-    int findSecondMinimumValue(TreeNode *root) {
-        set<int> st;
-        inorder(root, st);
-        if (st.size() <= 1)
-            return -1;
+    int findSecondMinimumValue(TreeNode* root) {
+        long long int first=LONG_MAX,second=LONG_MAX;
+        //do the inoder traversal of the binary tree and then find the answer
+        inorder(root,first,second);
 
-        auto it = st.begin();
-        it++;
-        return *it;
+        //if we are not able to find out any second smallest node
+        if(second==LONG_MIN)
+            return -1;
+        return second;
     }
 };
