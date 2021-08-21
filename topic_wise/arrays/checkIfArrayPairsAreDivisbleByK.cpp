7423ecff235ea62462b457fed7da25f551e76367
archit-1997 <architsingh456@gmail.com>
2021-08-21 08:10:09 +0530

    UPD : added solution for Check If  Arraa Pairs are Divisible by K

---
+++ topic_wise/arrays/checkIfArrayPairsAreDivisbleByK.cpp
@@ -0,0 +1,43 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : checkIfArrayPairsAreDivisbleByK.cpp
+ * @created     : Friday Aug 20, 2021 21:41:15 IST
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+
+        long long int n=arr.size();
+        unordered_map<long long int,long long int> count;
+        for(int i=0;i<n;i++){
+           long long int val=( ( arr[i]%k )+k )%k;
+            count[val]++;
+        }
+
+        for(auto it=count.begin();it!=count.end();it++){
+            //if the count of this element is greater than 0
+            long long int num=it->first;
+            if(num==0){
+                if(count[num]&1)
+                    return false;
+                continue;
+            }
+            if(it->second>0){
+                long long int rem=k-num;
+                if(count[rem]!=it->second)
+                    return false;
+                count[rem]-=it->second;
+                it->second=0;
+            }
+        }
+        return true;
+
+
+    }
+};
