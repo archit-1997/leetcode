0745b81cfa8c11129efe4573ccb2ed6fc13c527f
archit-1997 <architsingh456@gmail.com>
2021-08-28 16:41:32 +0530

    UPD : added recursive solution for Longest Common Subsequence

--- topic_wise/dynamicprogramming/longestCommonSubsequence.cpp
+++ topic_wise/dynamicprogramming/longestCommonSubsequence.cpp
@@ -1,40 +1,45 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : longestCommonSubsequence.cpp
+ * @created     : Saturday Aug 28, 2021 16:29:13 IST
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        //iterative solution
-        int n=text1.size(),m=text2.size();
-        vector<vector<int>> dp(n,vector<int> (m,0));
 
+    int solve(vector<vector<int>> &dp,string &text1,string &text2,int n,int m){
+        //if we have reached the end
+        if(n<=0 || m<=0)
+            return 0;
 
-        //populate the first row
-        bool flag=false;
-        for(int j=0;j<m;j++){
-          if(text2[j]==text1[0])
-              flag=true;
-          if(flag)
-            dp[0][j]=1;
+        //if already computed
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+
+        //if the current character for both of them is same
+        if(text1[n-1]==text2[m-1])
+            dp[n][m]=1+solve(dp,text1,text2,n-1,m-1);
+        //if current character different
+        else{
+            int a=solve(dp,text1,text2,n-1,m);
+            int b=solve(dp,text1,text2,n,m-1);
+
+            dp[n][m]=max(a,b);
         }
-
-        //populate the first col
-        flag=false;
-        for(int i=0;i<n;i++){
-          if(text1[i]==text2[0])
-              flag=true;
-          if(flag)
-            dp[i][0]=1;
-        }
+        return dp[n][m];
         
-        for(int i=1;i<text1.size();i++){
-            for(int j=1;j<text2.size();j++){
-               if(text1[i]==text2[j])
-                   dp[i][j]=dp[i-1][j-1]+1;
-               else
-                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n-1][m-1];
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size(),m=text2.size();
+
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+
+        return solve(dp,text1,text2,n,m);
     }
 };
