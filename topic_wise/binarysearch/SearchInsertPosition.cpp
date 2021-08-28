45b89727caebc4a4b81c6140a506f5d22c6f0e72
archit-1997 <architsingh456@gmail.com>
2021-08-28 22:43:16 +0530

    UPD : updated solution for Search Insert Position

--- topic_wise/binarysearch/SearchInsertPosition.cpp
+++ topic_wise/binarysearch/SearchInsertPosition.cpp
@@ -1,36 +1,18 @@
-/*Given a sorted array and a target value, return the index if the target is
-found. If not, return the index where it would be if it were inserted in order.
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : searchInsertPosition.cpp
+ * @created     : Saturday Aug 28, 2021 22:42:56 IST
+ */
 
-You may assume no duplicates in the array.
-
-Example 1:
-
-Input: [1,3,5,6], 5
-Output: 2
-Example 2:
-
-Input: [1,3,5,6], 2
-Output: 1
-Example 3:
-
-Input: [1,3,5,6], 7
-Output: 4
-Example 4:
-
-Input: [1,3,5,6], 0
-Output: 0*/
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
-  int searchInsert(vector<int> &nums, int target) {
-
-    // if number is present
-    if (binary_search(nums.begin(), nums.end(), target)) {
-      int ans = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-      return ans;
-    } else {
-      int ans = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-      return ans;
+    int searchInsert(vector<int>& nums, int target) {
+        int ans=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        return ans;
     }
-  }
 };
