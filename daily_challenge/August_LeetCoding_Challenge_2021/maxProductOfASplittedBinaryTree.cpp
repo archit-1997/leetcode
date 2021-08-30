38501d6c55b03ac4964fd5c22ac88b44a653f585
archit-1997 <architsingh456@gmail.com>
2021-08-30 20:41:00 +0530

    UPD : added solution for Maximum Product of a Splitted Binary Tree

--- daily_challenge/August_LeetCoding_Challenge_2021/maxProductOfASplittedBinaryTree.cpp
+++ daily_challenge/August_LeetCoding_Challenge_2021/maxProductOfASplittedBinaryTree.cpp
@@ -3,11 +3,19 @@
  * @GitHub      : archit-1997
  * @Email       : architsingh456@gmail.com
  * @file        : maxProductOfASplittedBinaryTree.cpp
- * @created     : Friday Aug 20, 2021 10:42:58 IST
+ * @created     : Monday Aug 30, 2021 20:21:11 IST
  */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+#define P pair<int,int>
+
+void init(){
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+}
 
   struct TreeNode {
       int val;
@@ -17,64 +25,42 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
- 
+
 class Solution {
 public:
 
-    int populateSum(TreeNode* root,unordered_map<int,int> &sum,int pos){
+    void findSum(TreeNode* root,long long &sum){
+        if(root==NULL)
+            return; 
+        findSum(root->left,sum);
+        sum+=root->val;
+        findSum(root->right,sum);
+    }
+
+    int findRes(TreeNode* root,long long &ans,long long &sum){
         if(root==NULL)
             return 0;
         
-        //finding the sum for the left child
-        int left=populateSum(root->left,sum,2*pos+1);
-        //finding the sum for the right child
-        int right=populateSum(root->right,sum,2*pos+2);
+        //do postorder traversal
+        long long ls=findRes(root->left,ans,sum);
+        long long rs=findRes(root->right,ans,sum);
 
-        int totalSum=left+right+root->val;
-        sum[pos]=totalSum;
-        return totalSum;
-    }
+        //find the left and right result
+        long long left=(ls)*(sum-ls);
+        long long right=(rs)*(sum-rs);
+        ans=max(ans,max(left,right));
 
-    bool isLeaf(TreeNode* root){
-        return (root->left==NULL && root->right==NULL);
-    }
-
-    int findProduct(unordered_map<int,int> &sum,int index,int totalSum){
-        int a=sum[index];
-        int b=totalSum-a;
-
-        return a*b;
-    }
-
-    void findRes(TreeNode* root,unordered_map<int,int> &sum, int pos,int totalSum,long long int &ans){
-        //at a particular root, we can disconnect the left and the right edge
-        if(root==NULL || isLeaf(root))
-            return;
-
-        //recur for the left and the right subtree
-        findRes(root->left,sum,2*pos+1,totalSum,ans);
-        findRes(root->right,sum,2*pos+2,totalSum,ans);
-
-        //now finding the max of the left and the right nodes
-        int leftVal=findProduct(sum,2*pos+1,totalSum);
-        int rightVal=findProduct(sum,2*pos+2,totalSum);
-
-        ans=max(leftVal,rightVal);
+        return (ls+rs+root->val);
     }
 
     int maxProduct(TreeNode* root) {
-        unordered_map<int,int> sum;
-        //first we need to store the value of the sum of the nodes
+        long long sum=0;
         TreeNode* tmp=root;
-        populateSum(tmp,sum,0);
+        findSum(tmp,sum);
 
-        //total sum will be the value of the root
-        int totalSum=sum[0];
-
-        long long int  ans=INT_MIN;
-
-        findRes(root,sum,0,totalSum,ans);
-
-        return ans%(int)(1e9+7);
+        long long ans=0;
+        findRes(root,ans,sum);
+        ans=ans%1000000007;
+        return ans;
     }
 };
