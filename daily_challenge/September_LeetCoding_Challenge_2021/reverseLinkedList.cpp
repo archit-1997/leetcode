b7f9f7bd6d03ca197c25a7c9f47a056f354da7f8
archit-1997 <architsingh456@gmail.com>
2021-09-08 10:04:22 +0530

    UPD : added solution for Reverse Linked List

---
+++ daily_challenge/September_LeetCoding_Challenge_2021/reverseLinkedList.cpp
@@ -0,0 +1,45 @@
+/**
+ * @author      : archit
+ * @GitHub      : archit-1997
+ * @Email       : architsingh456@gmail.com
+ * @file        : reverseLinkedList.cpp
+ * @created     : Wednesday Sep 08, 2021 10:01:45 IST
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define P pair<int,int>
+
+void init(){
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+
+        ListNode* prev=NULL,*cur=head,*next=NULL;
+        while(cur!=NULL){
+            next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
